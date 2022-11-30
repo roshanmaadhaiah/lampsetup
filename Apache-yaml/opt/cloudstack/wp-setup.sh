421944bc5da511fab4c4b15e4b7261c3f5c892e7
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-11-30 18:18:27 +0530

    Update wp-setup.sh

--- Apache-yaml/opt/cloudstack/wp-setup.sh
+++ Apache-yaml/opt/cloudstack/wp-setup.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+a=0
+while [ $a -eq 0 ]
+do
+ read -p "Domain/Subdomain name: " dom
+ if [ -z "$dom" ]
+ then
+  a=0
+  echo "Please provide a valid domain or subdomain name to continue to press Ctrl+C to cancel"
+ else
+  a=1
+fi
+done
+sed -i "s/\$domain/$dom/g"  /etc/apache2/sites-enabled/001-default.conf
+a2enconf block-xmlrpc
+systemctl restart apache2
