ae65a95670e464b4601b9f5bdb30e521e9f4d1b8
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-01 19:05:56 +0530

    Update wp-setup.sh

--- Apache-yaml/opt/cloudstack/wp-setup.sh
+++ Apache-yaml/opt/cloudstack/wp-setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# WordPress Salts
+for i in `seq 1 8`
+do
+    wp_salt=$(</dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()\-_ []{}<>~`+=,.;:/?|' | head -c 64 | sed -e 's/[\/&]/\\&/g')
+    sed -e "0,/put your unique phrase here/s/put your unique phrase here/${wp_salt}/" \
+        -i /var/www/html/wp-config.php;
+done
+
+#To replace the Domain Name in the apache configuration 
 a=0
 while [ $a -eq 0 ]
 do
