5accd3aa3c1cd7a621dee913951a8b3039187b24
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-12 22:03:57 +0530

    Update wp-setup.sh

--- Apache-yaml/opt/cloudstack/wp-setup.sh
+++ Apache-yaml/opt/cloudstack/wp-setup.sh
@@ -8,6 +8,21 @@ do
         -i /var/www/html/wp-config.php;
 done
 
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+> /root/.ssh/authorized_keys
+apt-get -y autoremove
+apt-get -y autoclean
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+
 #To replace the Domain Name in the apache configuration 
 a=0
 while [ $a -eq 0 ]
