0034432ae92bb095bfc824509bac83edf8c9730b
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-13 20:31:48 +0530

    Create joomla-setup.sh

---
+++ Apache-yaml/opt/cloudstack/joomla-setup.sh
@@ -0,0 +1,32 @@
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+> /root/.ssh/authorized_keys
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+
+#To replace the Domain Name in the apache configuration 
+a=0
+while [ $a -eq 0 ]
+do
+ read -p "Domain/Subdomain name: " dom
+ if [ -z "$dom" ]
+ then
+  a=0
+  echo "Please provide a valid domain or subdomain name to continue to press Ctrl+C to cancel"
+ else
+  a=1
+fi
+done
+sed -i "s/\$domain/$dom/g"  /etc/apache2/sites-available/001-default.conf
+systemctl restart apache2
+rm -rf /root/.bashrc
+cp /etc/skel/.bashrc /root
