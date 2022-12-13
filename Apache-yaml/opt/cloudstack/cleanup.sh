04cf9e722b8a7d992c463a6249bda39ac1c79c5b
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-13 16:30:48 +0530

    Create cleanup.sh

---
+++ Apache-yaml/opt/cloudstack/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+> /root/.ssh/authorized_keys
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+
+rm -rf /root/.bashrc
+cp /etc/skel/.bashrc /root
