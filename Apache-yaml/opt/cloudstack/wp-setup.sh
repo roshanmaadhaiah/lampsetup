2aeebac6f0cfce9f5cc18d45e9f085229f81c037
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-13 11:59:52 +0530

    Update wp-setup.sh

--- Apache-yaml/opt/cloudstack/wp-setup.sh
+++ Apache-yaml/opt/cloudstack/wp-setup.sh
@@ -17,8 +17,8 @@ find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
 rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
 cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
 > /root/.ssh/authorized_keys
-apt-get -y autoremove
-apt-get -y autoclean
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
 history -c
 cat /dev/null > /root/.bash_history
 unset HISTFILE
