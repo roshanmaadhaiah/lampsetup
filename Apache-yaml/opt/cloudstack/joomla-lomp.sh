1545d73899da81d9142eedc26e83bb892bc2e8d0
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-21 19:25:04 +0530

    Update joomla-lomp.sh

--- Apache-yaml/opt/cloudstack/joomla-lomp.sh
+++ Apache-yaml/opt/cloudstack/joomla-lomp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#To replace the Domain Name in the apache configuration 
+#To replace the Domain Name in the apache configuration
 a=0
 while [ $a -eq 0 ]
 do
@@ -13,3 +13,42 @@ do
   a=1
 fi
 done
+
+#Configuring new host
+ssli=ssl
+echo "
+virtualhost $dom {
+vhRoot /var/www/html/
+configFile /usr/local/lsws/conf/vhosts/joomla/vhconf.conf
+allowSymbolLink 1
+enableScript 1
+restrained 1
+setUIDMode 0
+}
+
+listener $dom {
+address *:80
+secure 0
+map $dom *, $dom, www.$dom
+}
+
+listener $dom$ssli {
+address *:443
+secure 1
+keyFile /usr/local/lsws/conf/example.key
+certFile /usr/local/lsws/conf/example.crt
+map $dom *, $dom, www.$dom
+}" | tee -a /usr/local/lsws/conf/httpd_config.conf >/dev/null
+
+#Adding Virtual host Entry
+echo "
+docRoot                   /var/www/html/
+
+index  {
+  useServer               0
+  indexFiles              index.php index.html
+}" | tee -a /usr/local/lsws/conf/vhosts/joomla/vhconf.conf >/dev/null
+
+systemctl restart lsws
+rm -rf /root/.bashrc
+cp /etc/skel/.bashrc /root
