33bd14caa014473fd2c0a57d9f371f807041e30c
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-21 17:56:23 +0530

    Update joomla-lomp.sh

--- Apache-yaml/opt/cloudstack/joomla-lomp.sh
+++ Apache-yaml/opt/cloudstack/joomla-lomp.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#To replace the Domain Name in the apache configuration 
+a=0
+while [ $a -eq 0 ]
+do
+ read -p "Domain/Subdomain name: " dom
+ if [ -z "$dom" ]
+ then
+  a=0
+  echo "Please provide a valid domain or subdomain name to continue to press Ctrl+C to cancel"
+ else
+  a=1
+fi
+done
