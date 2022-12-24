4192b943281406ef8333cbadfd519e187e1c02a3
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2022-12-24 19:04:59 +0530

    Update joomla-lomp.sh

--- Apache-yaml/opt/cloudstack/joomla-lomp.sh
+++ Apache-yaml/opt/cloudstack/joomla-lomp.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo
+echo -e "${RED}Use the Below Database Credentials to setup Joomla${NC}"
+echo
+cat /root/.joomla_database_details
+echo
+
+#Cleanup script
+rm -rf /usr/local/src/
+mkdir -p /usr/local/src/
+rm -rf /var/lib/cloud/instances/*
+rm -rf /var/lib/cloud/data/*
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+cat /dev/null > /var/log/lastlog; cat /dev/null > /var/log/wtmp
+> /root/.ssh/authorized_keys
+apt-get -y autoremove >/dev/null 2>&1
+apt-get -y autoclean >/dev/null 2>&1
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+
+
 #To replace the Domain Name in the apache configuration
 a=0
 while [ $a -eq 0 ]
