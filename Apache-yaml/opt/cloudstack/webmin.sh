30d91a70b6341b64d7f23f81ed5e404afe7bcca2
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2023-01-17 12:57:30 +0530

    Create webmin.sh

---
+++ Apache-yaml/opt/cloudstack/webmin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${RED}Webmin Credentials are stored under /root/.webmin_root_password${NC}"
+echo
+echo -e "${RED}Use the Below Webmin Credentials to login webmin Panel${NC}"
+echo
+cat /root/.webmin_root_password
+echo
