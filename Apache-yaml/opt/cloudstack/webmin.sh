2b0e02f50135bffdd3d48694d01466661152aaad
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2023-01-18 11:45:20 +0530

    Update webmin.sh

--- Apache-yaml/opt/cloudstack/webmin.sh
+++ Apache-yaml/opt/cloudstack/webmin.sh
@@ -3,6 +3,8 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
+echo -e "${RED}To login to Webmin Admin Panel:- http://IPADDRESS:10000${NC}"
+echo
 echo -e "${RED}Webmin Credentials are stored under /root/.webmin_root_password${NC}"
 echo
 echo -e "${RED}Use the Below Webmin Credentials to login webmin Panel${NC}"
