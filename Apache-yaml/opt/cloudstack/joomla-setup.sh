b103bf086438155172465ac09ed3de48f41f2b50
roshanmaadhaiah <114063087+roshanmaadhaiah@users.noreply.github.com>
2023-01-31 15:59:13 +0530

    Update joomla-setup.sh

--- Apache-yaml/opt/cloudstack/joomla-setup.sh
+++ Apache-yaml/opt/cloudstack/joomla-setup.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-RED='\033[0;31m'
+RED='\033[1;31m'
 NC='\033[0m'
 
+echo -e "${RED}
+################################################################################################################
+#                              Your MarketPlace App has been deployed successfully!                            #
+#                                 Passwords are stored under /root/                                            #
+#                                 Please spend 2 minutes for configuration                                     #
+#                              Kindly update the below configuration to complete the setup                     #
+################################################################################################################
+${NC}"
+
 echo
-echo -e "${RED}Use the Below Database Credentials to setup Joomla${NC}"
+echo -e "${RED}This message will be removed in the next login!${NC}"
+echo
+echo
+echo -e "${RED}Use the Below Password for logging into MySQL${NC}"
+echo
+cat /root/.mysql_root_password
+echo
+echo -e "${RED}Use the Below Password for logging into the Phpmyadmin${NC}"
+echo
+cat /root/.phpmyadmin_password
+echo
+echo -e "${RED}Refer to the below Joomla database login details${NC}"
 echo
 cat /root/.joomla_database_details
 echo
@@ -27,16 +47,22 @@ unset HISTFILE
 a=0
 while [ $a -eq 0 ]
 do
+ echo -e "${RED}To cancel setup, press Ctrl+C.  This script will run again on your next login:${NC}"
+ echo -e "${RED}Enter the domain name for your new Joomla site:${NC}"
+ echo -e "${RED}(ex. example.org or test.example.org) do not include www or http/s:${NC}"
  read -p "Domain/Subdomain name: " dom
  if [ -z "$dom" ]
  then
   a=0
-  echo "Please provide a valid domain or subdomain name to continue to press Ctrl+C to cancel"
+  echo -e "${RED}Please provide a valid domain or subdomain name to continue to press Ctrl+C to cancel${NC}"
  else
   a=1
 fi
 done
+
 sed -i "s/\$domain/$dom/g"  /etc/apache2/sites-available/001-default.conf
+
 systemctl restart apache2
+
 rm -rf /root/.bashrc
 cp /etc/skel/.bashrc /root
